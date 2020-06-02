3a5971bfcf3967b81cfc308d0290bfe7e9f75482
AmyWongCheese <mmwong@MMWONG-PC.jccsstm.local>
2020-06-02 11:25:43 +0800

    lemon tea

---
+++ models/Modelvitalemontea1.java
@@ -0,0 +1,31 @@
+// Made with Blockbench 3.5.2
+// Exported for Minecraft version 1.14
+// Paste this class into your mod and generate all required imports
+
+public static class Modelvitalemontea1 extends EntityModel {
+	private final ModelRenderer bone;
+
+	public Modelvitalemontea1() {
+		textureWidth = 16;
+		textureHeight = 16;
+
+		bone = new ModelRenderer(this);
+		bone.setRotationPoint(0.0F, 24.0F, 0.0F);
+		bone.cubeList.add(new ModelBox(bone, 0, 0, -2.0F, -6.0F, -1.0F, 4, 6, 2, 0.0F, false));
+	}
+
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		bone.render(f5);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+
+	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
+		super.setRotationAngles(f, f1, f2, f3, f4, f5, e);
+	}
+}
